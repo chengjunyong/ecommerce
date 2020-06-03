8704dd95d3f2ffeb1ce1c52d139a49fde92f7dee
Ethan <junyong1213@hotmail.com>
2020-06-03 19:00:18 +0800

    Report module layout completed 50 %

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -67,8 +67,13 @@
       @endif
 
       @if($logged_user->user_type == 1 || $logged_user->user_type == 2)
-      <li><a class="sidebar-header" href="{{ route('getReport') }}"><i data-feather="bar-chart"></i><span>Reports</span></a></li>
-      <li><a class="sidebar-header" href=""><i data-feather="settings" ></i><span>Settings</span><i class="fa fa-angle-right pull-right"></i></a>
+      <li><a class="sidebar-header" href=""><i data-feather="bar-chart"></i><span>Report</span><i class="fa fa-angle-right pull-right"></i></a>
+        <ul class="sidebar-submenu">
+          <li><a href="{{ route('getSpecifyDateReport') }}"><i class="fa fa-circle"></i>Specify Date Report</a></li>
+          <li><a href=""><i class="fa fa-circle"></i>Product Report</a></li>
+        </ul>
+      </li>      
+        <li><a class="sidebar-header" href=""><i data-feather="settings" ></i><span>Settings</span><i class="fa fa-angle-right pull-right"></i></a>
         <ul class="sidebar-submenu">
           <li><a href="{{ route('getProfile') }}"><i class="fa fa-circle"></i>Profile</a></li>
         </ul>
