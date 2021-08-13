269bb8b34f78665fdb460ade1bfa7cf286ee2d2e
ecommerce <wongwaiwen1001@gmail.com>
2021-08-13 23:31:43 +0800

    update login error

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -46,7 +46,7 @@ class LoginController extends Controller
     public function login(Request $request)
     {
       $credentials = $request->only('email', 'password');
-      $credentials["type"] = null;
+      $credentials["user_type"] = null;
 
       if (Auth::attempt($credentials)) {
           // Authentication passed...
@@ -69,7 +69,7 @@ class LoginController extends Controller
     public function adminLogin(Request $request)
     {
       $credentials = $request->only('email', 'password');
-      $credentials["type"] = 1;
+      $credentials["user_type"] = 1;
 
       if (Auth::guard('admin')->attempt($credentials)) {
           // Authentication passed...
