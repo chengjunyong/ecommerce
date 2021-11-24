9b7a973e81ba030fc74b54e1cd163c3c7c326eaf
chengjunyong <junyong1213@hotmail.com>
2021-11-24 17:29:13 +0800

    Integrate Google Analytics

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -87,6 +87,16 @@
     window.FontAwesomeConfig = { autoReplaceSvg: false }
   </script>
 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-8NSH949G3D"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-8NSH949G3D');
+  </script>
+
 </head>
 <body style="background: #f8f9fa;">
 
